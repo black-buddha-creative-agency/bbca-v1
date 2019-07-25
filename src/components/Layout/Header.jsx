85fe2d782fd5f0b0326dbd9ed74ebf7f0aa216b3
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-07-25 00:41:19 -0500

    Add data dir, update todos for index.css, menu.js, and Header.jsx

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import {NavLink, Link } from 'react-router-dom'
+import NavItem from './NavItem'
 
-const Header = () => {
-    return (  );
+const Header = (props) => {
+    return ( 
+        <ul>
+            // TODO map over a list of menu items from 
+        </ul>
+     );
 }
  
 export default Header;
---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+// TODO: Add menu data using the following structure:
+
+// const linkClass = ''
+// const menu = [
+//   {
+//     children: '',
+//     className: linkClass,
+//     to: '',
+//   },
+// ]
