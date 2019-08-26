42e0ff20bdf39bfd443b441673c05553d87839c9
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-08-26 01:50:38 -0500

    Update contact information to match live site

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,31 +2,31 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <section className="section" id="Contact">
-      <div className="section-container footer-bottom">
-        <div className="grid-layout grid-container--content">
-          <div className="grid-block--title">
-            <h1 className="text-h2">Contact</h1>
+    <section class="section" id="Contact">
+      <div class="section-container footer-bottom">
+        <div class="grid-layout grid-container--content">
+          <div class="grid-block--title">
+            <h1 class="text-h2">Contact</h1>
           </div>
-          <div className="grid-block--content">
-            <p className="text_p2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              possimus exercitationem velit sapiente quaerat mollitia omnis
-              porro aliquam, nemo quos rem dolorum temporibus beatae
-              dignissimos.
+          <div class="grid-block--content">
+            <p class="text_p2">
+              We are located in the historic Third Ward area of Houston and
+              surrounded by cafes, bars, and creative businesses. Contact us to
+              arrange a quick chat about your next event or to view selected
+              works.
               <br />
               <br />
               Call us
               <br />
-              <a className="text_p2 event-link" href="tel:5555555555">
-                555 555 5555
+              <a class="text_p2 event-link" href="tel:7135452011">
+                713 545 2011
               </a>
               <br />
               <br />
               Email us
               <br />
               <a
-                className="text_p2 event-link"
+                class="text_p2 event-link"
                 href="mailto:india@blackbuddha.agency"
               >
                 india@blackbuddha.agency
