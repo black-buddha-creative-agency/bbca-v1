005eb42828a19cea0d21a9c7cea7d7289f1fda2f
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-09-05 17:57:41 -0500

    Update

--- src/pages/SignUpPage/index.jsx
+++ src/pages/SignUpPage/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Section from '../../components/Layout/Section'
 import Header from '../../components/Layout/Header'
--- src/services/index.js
+++ src/services/index.js
@@ -7,15 +7,22 @@ module.exports = {
   // getAllSponsors
 }
 
+// returns a list of created Events by BBCA
 function getAllEvents() {
   return fetch(`${url}/events`).then(res => res.json())
 }
+
+// returns a list of Artists show by BBCA and collaborators
 function getAllArtists() {
   return fetch(`${url}/artists`).then(res => res.json())
 }
+
+// returns a list of all Curators from BBCA
 function getAllCurators() {
   return fetch(`${url}/curators`).then(res => res.json())
 }
+
+// returns a list of Sponsors in collaboration with BBCA
 function getAllSponsors() {
   return fetch(`${url}/sponsors`).then(res => res.json())
 }
