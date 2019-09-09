2401d22ef0f81d7ed1a90490b2d0a0ed98f435e0
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-09-09 00:48:36 -0500

    update

--- src/pages/AdminPage/index.jsx
+++ src/pages/AdminPage/index.jsx
@@ -2,21 +2,22 @@ import React, { Component } from 'react'
 import { BrowserRouter as Route, Link } from 'react-router-dom'
 import Contact from '../../components/Contact'
 import Header from '../../components/Layout/Header'
-
-const Test = ({ match }) => {
-  return <div className="tc">{match.params.id}</div>
-}
+import menu from '../../data/menu'
 
 class AdminPage extends Component {
   state = {}
+  dashboardMenu = menu.slice(0, 2)
   render() {
     return (
       <div className="tc mt5">
         <h1 className="text-h2">Admin Panel</h1>
         <div className="mt3 pa3 flex flex-row justify-between">
           <Link to={this.props.match.url}>Dashboard</Link>
-          <Link to={`${this.props.match.url}/events`}>Events</Link>
-          <Link to={`${this.props.match.url}/artists`}>Artists</Link>
+          {this.dashboardMenu.map(menuItem => (
+            <Link to={`${this.props.match.url}/${menuItem.children}`}>
+              {menuItem.children}
+            </Link>
+          ))}
         </div>
       </div>
     )
