58f53fd03a3b89c86ab60ee46149036b36409c86
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-12-11 00:02:50 -0600

    Update router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-import Layout from './components/Layout'
-import HomePage from './pages/HomePage'
-import EventsPage from './pages/EventsPage'
-import EventDetailPage from './pages/EventDetailPage'
-import ArtistsPage from './pages/ArtistsPage'
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import EventsPage from "./pages/EventsPage";
+import EventDetailPage from "./pages/EventDetailPage";
+import ArtistsPage from "./pages/ArtistsPage";
 
 const client = new ApolloClient({
-  uri: 'https://api.blackbuddhacreativeagency.com/graphql'
-})
+  uri: "https://api.blackbuddhacreativeagency.com/graphql"
+});
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <Router>
+      <Switch>
         <Layout>
           <Route exact path="/" component={HomePage} />
           <Route path="/events" component={EventsPage} />
           <Route path="/event/:slug" component={EventDetailPage} />
           <Route path="/artists" component={ArtistsPage} />
         </Layout>
-      </Router>
+      </Switch>
     </ApolloProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
