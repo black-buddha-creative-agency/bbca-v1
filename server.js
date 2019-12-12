8a6b17dd3decd567d3e95f5adf08934d023baf15
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-12-11 20:34:50 -0600

    Add server.js to serve project with node.js

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-const express = require('express');
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+
+app.use(favicon(path.join(__dirname, '')))
