4c1d54bfecd4b0d2f63ce8766c0275ddc632a69a
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2021-01-11 10:06:02 -0600

    Update

--- src/hooks/useApiUrl.js
+++ src/hooks/useApiUrl.js
@@ -4,11 +4,11 @@ const useApiUrl = (location) => {
 	if (location && location.hostname.indexOf("localhost") > -1) {
 		API_URL = "http://localhost:1337";
 	} else if (location && location.hostname.indexOf("dev") > -1) {
-		API_URL = "https://dev-api.blackbuddha.agency";
+		API_URL = "http://dev-api.blackbuddha.agency";
 	} else if (location && location.hostname.indexOf("staging") > -1) {
-		API_URL = "https://staging-api.blackbuddha.agency";
+		API_URL = "http://staging-api.blackbuddha.agency";
 	} else {
-		API_URL = "https://api.blackbuddha.agency";
+		API_URL = "http://api.blackbuddha.agency";
 	}
 
 	return { API_URL };
